5cfbf206aff7002740f5384d4f3fe3b68359b749
judelt <mendez.puig@gmail.com>
2021-06-03 17:41:07 -0700

    add validation for empty todos

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -40,10 +40,12 @@ export class TodosComponent implements OnInit {
   }
 
   addTodo() {
-    this.todos.push({
-      content: this.inputTodo,
-      completed: false,
-    });
+    if (this.inputTodo !== '') {
+      this.todos.push({
+        content: this.inputTodo,
+        completed: false,
+      });
+    }
     this.inputTodo = '';
   }
 }
